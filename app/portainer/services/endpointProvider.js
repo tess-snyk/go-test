83586ac324a46767126372fb08b200baf46fe16c
Chaim Lev-Ari <chiptus@gmail.com>
2022-01-16 15:17:37 +0200

    refactor(environment): ping endpoint

--- app/portainer/services/endpointProvider.js
+++ app/portainer/services/endpointProvider.js
@@ -1,19 +1,5 @@
-/* 
-initialize
-
-currentEndpoint
-endpoint
-
-endpointID,
-setEndpointID
-
-
-
-
-
-clean
-
-*/
+import { ping } from '@/docker/services/ping';
+import { PortainerEndpointTypes } from '../models/endpoint/models';
 
 angular.module('portainer.app').factory('EndpointProvider', EndpointProvider);
 
@@ -21,6 +7,7 @@ angular.module('portainer.app').factory('EndpointProvider', EndpointProvider);
 function EndpointProvider() {
   const state = {
     currentEndpoint: null,
+    pingInterval: null,
   };
 
   return { endpointID, setCurrentEndpoint, currentEndpoint, clean };
@@ -31,6 +18,15 @@ function EndpointProvider() {
 
   function setCurrentEndpoint(endpoint) {
     state.currentEndpoint = endpoint;
+
+    if (state.pingInterval) {
+      clearInterval(state.pingInterval);
+      state.pingInterval = null;
+    }
+
+    if (endpoint && endpoint.Type == PortainerEndpointTypes.EdgeAgentOnDockerEnvironment) {
+      state.pingInterval = setInterval(() => ping(endpoint.Id), 60 * 1000);
+    }
   }
 
   function currentEndpoint() {
@@ -38,6 +34,6 @@ function EndpointProvider() {
   }
 
   function clean() {
-    state.currentEndpoint = null;
+    setCurrentEndpoint(null);
   }
 }
