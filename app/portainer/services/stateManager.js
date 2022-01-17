sanyamason/portainer

feat(demo): disable features on demo env [EE-1874]
feat(demo): add demo flag

feat(backup): limit backup on demo env

feat(demo): disable update user and endpoint on demo

feat(demo): fetch demo status to FE

feat(users): disable user update password on demo

feat(settings): disable update logo/telemtry on demo

app/portainer/services/stateManager.js
@@ -79,6 +79,7 @@ angular.module('portainer.app').factory('StateManager', [
       state.application.version = status.Version;
       state.application.edition = status.Edition;
       state.application.instanceId = status.InstanceID;
+      state.application.isDemo = status.DemoEnvironment;
 
       state.application.enableTelemetry = settings.EnableTelemetry;
       state.application.logo = settings.LogoURL;

tess-snyk/go-test

app/portainer/services/stateManager.js
@@ -88,6 +88,8 @@ function StateManagerFactory(
     state.application.version = status.Version;
     state.application.edition = status.Edition;
     state.application.instanceId = status.InstanceID;
+    state.application.demoEnvironment = status.DemoEnvironment;
+    state.application.demoDetails = status.DemoDetails;
 
     state.application.enableTelemetry = settings.EnableTelemetry;
     state.application.logo = settings.LogoURL;